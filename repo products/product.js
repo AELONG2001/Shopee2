bc50950d86d41557680279dee7cf7ca520b800d3
longdesigner <longhk911@gmail.com>
2021-10-09 21:23:16 -0700

    add add

--- repo products/product.js
+++ repo products/product.js
@@ -41,3 +41,44 @@ function handleClickBtnImg() {
 }
 
 handleClickBtnImg()
+
+
+
+// handle click change size
+function handleChangeClickSize() {
+   const btnProductSize = $$('.product1__size-main')
+   btnProductSize.forEach((btn) => {
+       btn.addEventListener('click', () => {
+           btnProductSize.forEach(e => e.classList.remove('active'))
+           btn.classList.add('active')
+       })
+   })
+}
+
+handleChangeClickSize()
+
+
+// handle click add and subtract input
+function handleAddSubtractInput() {
+   const addInput = $('.product1__amount-main-plus')
+   const subtractInput = $('.product1__amount-main-minus')
+   const inputProduct = $('.product1__amount-main-input-content')
+
+   addInput.addEventListener('click', function() {
+    inputProduct.value++
+   })
+
+   subtractInput.addEventListener('click', function() {
+    inputProduct.value--
+    if(inputProduct.value < 1) {
+        inputProduct.value = 1
+    }
+   })
+
+   inputProduct.addEventListener('input', () => {
+       
+   })
+
+}
+
+handleAddSubtractInput()
